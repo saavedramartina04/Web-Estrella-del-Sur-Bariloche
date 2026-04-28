dc9121a21ea24230f58cf1ae3302a384c927a424
saavedramartina04 <saavedramartina04@gmail.com>
2026-04-27 21:15:25 -0300

    modificado

--- js/productos.js
+++ js/productos.js
@@ -1,15 +1,46 @@
 const productos = [
-  {
-    nombre: 'Notebook Lenovo IdeaPad 3',
-    precio: 850000,
+   {
+    nombre: 'Camiseta Roja',
+    precio:25000,
     stock: 12,
-    marca: "Lenovo",
-    categoria: "Notebook",
-    detalles: "Intel i5, 8GB RAM, SSD 512GB, pantalla 15.6''",
+    marca: "FM Sport",
+    categoria: "Camiseta",
+    detalles: "Camiseta roja con rayos blancos",
     envio: true,
-    foto: "https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04",
+    foto: "img/camiseta-roja-blanca.png",
 
   },
+   {
+    nombre: 'Pantalón Rojo',
+    precio:20000,
+    stock: 11,
+    marca: "FM Sport",
+    categoria: "Short",
+    detalles: "Short rojo",
+    envio: true,
+    foto: "img/pantalon-roja-blanca.png",
 
- 
+  },
+   {
+    nombre: 'Medias Rojas',
+    precio:10000,
+    stock: 25,
+    marca: "FM Sport",
+    categoria: "Medias",
+    detalles: "Medias rojas con escudo",
+    envio: true,
+    foto: "img/medias.png",
+
+  },
+    {
+    nombre: 'Camiseta blanca',
+    precio:25000,
+    stock: 12,
+    marca: "FM Sport",
+    categoria: "Camiseta",
+    detalles: "Camiseta blanca con rayos grises",
+    envio: true,
+    foto: "img/camiseta-blanca.png",
+
+  },
 ];
